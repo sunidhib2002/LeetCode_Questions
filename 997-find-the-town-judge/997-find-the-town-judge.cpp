e17bd67e20446a1afbafecdabf13726c6608ce97
Sunidhi Bansal <77052786+sunidhib2002@users.noreply.github.com>
2022-06-24 17:26:08 +0530

    Time: 331 ms (22.01%), Space: 60.9 MB (66.26%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.cpp
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -1,42 +1,23 @@
 class Solution {
 public:
-    bool isPath(int source, int destination, unordered_map<int, vector<int>> &adj) {
-        vector<int> ngb = adj[source];
-        for (int i = 0; i < ngb.size(); i++) {
-            if (ngb[i] == destination) {
-                return true;
-            }
-        }
-        return false;
-    }
-    
-    int findJudge(int n, vector<vector<int>>& trust) {    
-        unordered_map<int, vector<int>> adj;
+
+    int findJudge(int n, vector<vector<int>>& trust) { 
+        vector<int> inDegree(n + 1, 0);
+        vector<int> outDegree(n + 1, 0);
+        
         for (int i = 0; i < trust.size(); i++) {
             int node1 = trust[i][0];
             int node2 = trust[i][1];
-            adj[node1].push_back(node2);
+            inDegree[node2]++;
+            outDegree[node1]++;
         }
         
-        vector<int> possible_town_judges;
         for (int i = 1; i <= n; i++) {
-            if (adj.find(i) == adj.end()) {
-                possible_town_judges.push_back(i);
+            if (outDegree[i] == 0 && inDegree[i] == n - 1) {
+                return i;
             }
         }
-        
-        int town_judge = -1;
-        for (int j = 0; j < possible_town_judges.size(); j++) {
-            int val = possible_town_judges[j];
-            town_judge = val;
-            for (int i = 1; i <= n; i++) {
-                if (i != val && isPath(i, val, adj) == false) {
-                    town_judge = -1;
-                    break;
-                }
-            }
-        }
-        
-        return town_judge;
+    
+        return -1;
     }
 };
