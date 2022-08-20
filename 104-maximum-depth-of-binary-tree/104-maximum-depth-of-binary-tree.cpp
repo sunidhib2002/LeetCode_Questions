c7958e4fb84ecd5d7759e061d22d6be55ed3d91f
Sunidhi Bansal <77052786+sunidhib2002@users.noreply.github.com>
2022-08-20 09:57:22 +0530

    Time: 20 ms (21.37%), Space: 18.9 MB (55.78%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
@@ -14,11 +14,11 @@ public:
     int maxDepth(TreeNode* root) {
         if (root == NULL) {
             return 0;
-        } 
+        }
         
-        int heightLeft = maxDepth(root -> left);
-        int heightRight = maxDepth(root -> right);
+        int left = maxDepth(root -> left);
+        int right = maxDepth(root -> right);
         
-        return max(heightLeft, heightRight) + 1;
+        return 1 + max(left, right);
     }
 };
