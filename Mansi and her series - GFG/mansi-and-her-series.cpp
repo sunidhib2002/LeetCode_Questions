6e72acc6bd4861f4d47269a0aaa385bba3f87289
Sunidhi Bansal <77052786+sunidhib2002@users.noreply.github.com>
2022-11-22 18:54:51 +0530

    Added solution - LeetHub

--- Mansi and her series - GFG/mansi-and-her-series.cpp
+++ Mansi and her series - GFG/mansi-and-her-series.cpp
@@ -7,20 +7,38 @@ using namespace std;
 //User function Template for C++
 class Solution {
   public:
-  bool isPrime(int n){if(n<=1)return false;if(n<=3)return true;if(n%2==0 || n%3==0 )return false;for(int i=5;i*i<=n;i=i+6){if(n%i==0 || n%(i+2)==0)return false;}return true;}
-
+    bool isPrime(int n) {
+        if (n < 2) {
+            return false;
+        }    
+        if (n == 2 || n == 3) {
+            return true;
+        }
+        if (n % 2 == 0 || n % 3 == 0) {
+            return false;
+        }
+        for (int i = 5; i * i <= n; i+= 6) {
+            if (n % i == 0 || n % (i + 2) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
     int nthTerm(int n){
-       int count=1;
-	int ans=0;
-	for(int i=2;count<=n;i++){
-		if(isPrime(i)){
-			count++;
-			ans=i;
-		}
-
-
-	}
-	return (ans+1)*n;
+        int i;
+        int ans = 0;
+        int m = n;
+        for (i = 2; m != 0; i++) {
+            if (isPrime(i)) {
+                m--;
+                ans = i;
+            }
+        }
+    
+        int a = (ans * n) + n;
+        return a;
+        // code here
     }
 };
 
